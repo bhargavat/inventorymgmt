d744d9086f61ea627b20b7b30b8d1a957af29dc7
Bhargava Ramisetty <bhargava.ramisetty@gmail.com>
2017-03-29 17:42:09 -0700

    Initial commit

---
+++ application/views/pages/login.php
@@ -0,0 +1,10 @@
+ <div class="jumbotron vertical-center" id="loginform">
+        <h3 class="header">Login</h3>
+        <form action="" method="post">
+            <label for="email">Email</label><br>
+            <input type="text" name="email" class="form-control" placeholder="Email"><br>
+            <label for="pwd">Password</label><br>
+            <input type="password" name="pwd" class="form-control" placeholder="Password"><br><br>
+            <input class="btn btn-default" type="submit" value="Login">
+        </form>
+    </div>
---
+++ application/views/templates/header.php
@@ -0,0 +1,24 @@
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>BackRoom</title>
+        <link rel="stylesheet" href="http://bootswatch.com/flatly/bootstrap.min.css">
+    </head>
+    <body>
+        <nav class="navbar navbar-inverse">
+            <div class="container">
+                <div class="navbar-header">
+                    <a class="navbar-brand" href="<?php echo base_url(); ?>">BackRoom</a>
+                </div>
+                <div id="navbar">
+                    <ul class="nav navbar-nav">
+                        <li><a href="<?php echo base_url(); ?>about">About</a></li>
+                    </ul>
+                    <ul class="nav navbar-nav navbar-right">
+                        <li><a href="<?php echo base_url(); ?>login">Login</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+
+        <div class="container">
